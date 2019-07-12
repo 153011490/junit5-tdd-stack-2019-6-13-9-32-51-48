fa9a6a305b9082c0486f098222d3b973aaef9f47
SamsonLin <153011490@qq.com>
2019-07-12 20:52:20 +0800

    refactor:reduce computation
    I refactor the code to reduce computation and redundance.

--- src/main/java/com/thoughtworks/tdd/NumberGame.java
+++ src/main/java/com/thoughtworks/tdd/NumberGame.java
@@ -2,22 +2,13 @@ package com.thoughtworks.tdd;
 
 public class NumberGame {
     public static String number(int i){
-        if(i%3==0||i==13){
-            if(i%5==0){
-                if(i%7==0)return "FizzBuzzWhizz";
-                return "FizzBuzz";
-            }
-            if(i%7==0)return "FizzWhizz";
-
-            return "Fizz";
-        }
-        if(i%5==0){
-            if(i%7==0)return "BuzzWhizz";
-            return "Buzz";
-        }
-        if(i%7==0){
-            return "Whizz";
-        }
+        if(i%105==0)return "FizzBuzzWhizz";
+        if(i%35==0)return "BuzzWhizz";
+        if(i%21==0)return "FizzWhizz";
+        if(i%15==0)return "FizzBuzz";
+        if(i%7==0)return "Whizz";
+        if(i%5==0)return "Buzz";
+        if(i%3==0||i==13)return "Fizz";
         return ""+i;
     }
 }
